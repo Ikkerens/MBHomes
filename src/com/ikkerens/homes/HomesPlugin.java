c908bd213452e8908547239b980cb71ec7f3ff4c
Ikkerens <ikkerens@hotmail.com>
2014-06-27 00:06:52 +0200

    Initial commit

---
+++ src/com/ikkerens/homes/HomesPlugin.java
@@ -0,0 +1,12 @@
+package com.ikkerens.homes;
+
+import com.mbserver.api.MBServerPlugin;
+import com.mbserver.api.Manifest;
+
+@Manifest( name = "MBHomes" )
+public class HomesPlugin extends MBServerPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+}
